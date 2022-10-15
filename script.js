11d33cf308da5cb0b53d874dff797396d509abd2
Ovi Hasan <ovihasan074@gmail.com>
2022-10-15 11:14:30 +0600

    Update script.js with weather api key
    Update script.js with weather api key

---
+++ script.js
@@ -0,0 +1,18 @@
+let weather = {
+    apiKey: "728df2c50a3967c810967f49b6d29138",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
